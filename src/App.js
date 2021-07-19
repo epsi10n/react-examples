e8253256b40ec4f89277e4d308546c7a0d95023e
zhuravlev_kd <zhuravlev_kd@nlmk.com>
2021-07-19 13:15:56 +0300

    refactoring

--- src/App.js
+++ src/App.js
@@ -1,36 +1,13 @@
 import React from "react";
-import ThemedButton from "./scenes/CtxExamples/ThemeContext/themed-button";
-import {themes, ThemeContext} from "./scenes/CtxExamples/ThemeContext/theme-context";
-
-function Toolbar(props) {
-    return (
-        <ThemedButton onClick={props.changeTheme}>
-            Change Theme
-        </ThemedButton>);
-}
+import ThemeContextExample from "./scenes/CtxExamples/ThemeContext/theme-context-example";
 
 class App extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            theme: themes.light,
-        };
-
-        this.toggleTheme = () => {
-            this.setState(state => ({
-                theme: state.theme === themes.dark ? themes.light : themes.dark,
-            }));
-        };
-    }
-
     render() {
         return (
             <>
-                <ThemeContext.Provider value={this.state.theme}>
-                    <Toolbar changeTheme={this.toggleTheme} />
-                </ThemeContext.Provider>
+                <ThemeContextExample/>
             </>);
-}
+    }
 }
 
 export default App;
---
+++ src/scenes/CtxExamples/ThemeContext/theme-context-example.js
@@ -0,0 +1,28 @@
+import React, {useState} from "react";
+import ThemedButton from "./themed-button";
+import {themes, ThemeContext} from "./theme-context";
+
+function Toolbar(props) {
+    return (
+        <ThemedButton onClick={props.changeTheme}>
+            Change Theme
+        </ThemedButton>);
+}
+
+function ThemeContextExample() {
+    const [theme, setTheme] = useState(themes.light);
+
+    function toggleTheme() {
+        setTheme(theme === themes.dark ? themes.light : themes.dark);
+    }
+
+    return (
+        <>
+            <ThemeContext.Provider value={theme}>
+                <Toolbar changeTheme={toggleTheme} />
+            </ThemeContext.Provider>
+        </>);
+}
+
+export default ThemeContextExample;
+
